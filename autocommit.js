2370f7c32c302e0e873ef757084a70d94a32b2a0
Max Nowack <max@unsou.de>
2014-07-01 23:22:01 +0200

    added basic logic

--- autocommit.js
+++ autocommit.js
@@ -1,10 +1,44 @@
-var fs = require('fs');
+var repositories = [
+  "/Users/mnowack/Documents/testrepo/"
+];
 
-fs.watch('./', function (event, filename) {
-  console.log('event is: ' + event);
-  if (filename) {
-    console.log('filename provided: ' + filename);
-  } else {
-    console.log('filename not provided');
-  }
+
+var fs = require('fs');
+var git = require('gift');
+
+
+var processChange = function (repo,event, filename) {
+  repo.status(function(err,status){
+    if(status.clean) return;
+    repo.add(repo.path,function(err,files){
+      if(err) throw err;
+
+      repo.commit(filename,function(err){
+        if(err) throw err;
+        console.log("new commit: " + filename + " (" + repo.path + ")");
+      });
+    })
+  });
+}
+
+repositories.forEach(function(item){
+  fs.exists(item + "/.git/",function(exists){
+    if(!exists){
+      git.init(item,function(err,_repo){
+        if(err) throw err;
+        console.log("git init: " + item);
+        fs.watch(item, function(event,filename){
+          processChange(_repo,event,filename);
+        });
+        console.log("watcher started: " + item);
+        processChange(_repo,"","general");
+      });
+    }else{
+      fs.watch(item, function(event,filename){
+        processChange(git(item),event,filename);
+      });
+      console.log("watcher started: " + item);
+      processChange(git(item),"","general");
+    }
+  });
 });
